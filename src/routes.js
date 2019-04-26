d91b60af5b0c5ebe548025cb7ac9a9a4e1376f76
St Kol <st.kolodych@gmail.com>
2019-04-26 16:46:16 +0300

    preventRoutes

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,25 @@ import SignUp from './components/User/signup.vue'
 import SignIn from './components/User/signin.vue'
 import Dashboard from './components/User/dashboard.vue';
 
+import store from './store';
+
 Vue.use(VueRouter)
 
+const preventRoutes = {
+    beforeEnter: (to, from, next) => {
+        if(store.state.token) {
+            next();
+        } else {
+            next('/');
+        }
+    } 
+}
+
 const routes = [
-    { path:'/',component:Home },
+    { path:'/', component:Home },
     { path:'/signin', component: SignIn },
     { path:'/signup', component: SignUp },
-    { path:'/dashboard', component: Dashboard },
+    { path:'/dashboard', component: Dashboard, ...preventRoutes},
 ]
 
 export default new VueRouter({mode: 'history', routes})
